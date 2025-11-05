3bc327c34bfb60c13459a6c06f6c7ee2a41efa1e
stanleywijayaa <stanleytan2018@gmail.com>
2025-11-05 19:51:04 +0800

    created login page

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+// ignore: unused_import
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class LoginPage extends StatefulWidget{
+  const LoginPage({super.key});
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  _LoginPageState();
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
+
