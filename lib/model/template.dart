83f15ce8590ffa139a75686ab3df2d771ab5c19f
Realxeng <realxengpandelaki@gmail.com>
2025-11-07 18:45:46 +0800

    Make pdf file optional property of template

--- lib/model/template.dart
+++ lib/model/template.dart
@@ -3,23 +3,22 @@ import 'dart:typed_data';
 class Template {
   final int id;
   final String title;
-  final Uint8List pdfFile;
+  final Uint8List? pdfFile;
   final String tableName;
   final int fileSize;
 
   Template({
     required this.id,
     required this.title,
-    required this.pdfFile,
+    this.pdfFile,
     required this.tableName,
     required this.fileSize,
   });
 
-  factory Template.fromJson(Map<String, dynamic> json, file) {
+  factory Template.fromJson(Map<String, dynamic> json) {
     return Template(
       id: json['id'],
       title: json['title'],
-      pdfFile: file,
       tableName: json['table_name'],
       fileSize: json['size'],
     );
--- lib/services/template_logic.dart
+++ lib/services/template_logic.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:pdf_handler/model/template.dart';
 
-final nodeURL = 'http://localhost:3000';
+final nodeURL = 'http://127.0.0.1:3000';
 final nocoURL = 'https://connect.appnicorn.com';
 
 class TemplateLogic {
@@ -19,15 +19,8 @@ class TemplateLogic {
     final data = json.decode(detailsResponse.body);
     List details = data['data'];
 
-    final futures = details.map((detail) async {
-      final fileResponse = await http.get(
-        Uri.parse('$nocoURL${detail['url']}'),
-      );
-      final file = fileResponse.bodyBytes;
-      return Template.fromJson(detail, file);
-    });
-
-    final templates = await Future.wait(futures);
-    return templates;
+    return details.map((detail) {
+      return Template.fromJson(detail);
+    }).toList();
   }
 }
