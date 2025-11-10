933daf2c43e9fad4e25c700d0280bfc7653f62b7
Realxeng <realxengpandelaki@gmail.com>
2025-11-10 12:31:41 +0800

    Add create template page

---
+++ lib/pages/create_template.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CreateTemplate extends StatefulWidget {
+  const CreateTemplate({super.key});
+  @override
+  State<CreateTemplate> createState() => _CreateTemplateState();
+}
+
+class _CreateTemplateState extends State<CreateTemplate> {
+  @override
+  Widget build(BuildContext build) {
+    return Scaffold();
+  }
+}
