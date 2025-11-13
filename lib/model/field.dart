e782299b1e7876e2a5cb7c42bb286090374a4ebd
stanleywijayaa <stanleytan2018@gmail.com>
2025-11-13 18:34:05 +0800

    add page to field

--- lib/model/field.dart
+++ lib/model/field.dart
@@ -2,6 +2,7 @@ class Field {
   final String? type;
   final String fieldName;
   final String dataField;
+  int page;
   double x;
   double y;
   double width;
@@ -15,11 +16,13 @@ class Field {
     this.y = 0,
     this.height = 0,
     this.width = 0,
+    this.page = 1
   });
 
-  void move(double x, double y) {
+  void move(double x, double y, int? page) {
     this.x = x;
     this.y = y;
+    this.page = page ?? this.page;
   }
 
   void resize(double width, double height) {
@@ -32,12 +35,14 @@ class Field {
     double? y,
     double? height,
     double? width,
+    int? page,
     String? fieldName,
     String? dataField,
   }) {
     return Field(
       fieldName: fieldName ?? this.fieldName,
       dataField: dataField ?? this.dataField,
+      page: page ?? this.page,
       x: x ?? this.x,
       y: y ?? this.y,
       height: height ?? this.height,
