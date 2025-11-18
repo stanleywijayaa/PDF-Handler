268f1c055c75d22fd7985c6bc678a8a3dbfb21f7
Realxeng <realxengpandelaki@gmail.com>
2025-11-18 13:38:11 +0800

    Prevent stuck in loading screen when login failed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -179,6 +179,7 @@ class _LoginPageState extends State<LoginPage> {
       ScaffoldMessenger.of(
         context,
       ).showSnackBar(SnackBar(content: Text("Error: $e")));
+      Navigator.of(context, rootNavigator: true).pop();
     }
   }
 }
